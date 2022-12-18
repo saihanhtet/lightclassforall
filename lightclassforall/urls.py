914b1fa38a53d1a6cbc5954bdf2c6818d4d47463
IvanIsNotAvailable <hanhtetsan13@gmail.com>
2022-12-18 13:25:42 +0630

    second commit

--- lightclassforall/urls.py
+++ lightclassforall/urls.py
@@ -19,6 +19,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('forest', include('django_forest.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
     path('',include('Class.urls')),
 ]
