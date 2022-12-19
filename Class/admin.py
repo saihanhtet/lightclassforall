7ae122d062992dac65bc5a9944bf336876f9561a
SaiHanHtet <111681901+IvanIsNotAvailable@users.noreply.github.com>
2022-12-19 16:15:10 +0630

    Delete Class directory

--- Class/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(Course)
-admin.site.register(Subject)
-admin.site.register(Student)
-admin.site.register(Query)
-admin.site.register(filterstudent)
